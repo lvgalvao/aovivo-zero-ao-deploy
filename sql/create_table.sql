af6cf78527446a3648bb25ba09c27f00bcf79c5d
lvgalvaofilho <lvgalvaofilho@gmail.com>
2024-03-13 19:00:27 -0300

    update

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -7,3 +7,12 @@ CREATE TABLE vendas (
   categoria VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE vendas_calculado (
+  venda_id SERIAL PRIMARY KEY,
+  data_venda DATE NOT NULL,
+  valor DECIMAL(10, 2) NOT NULL,
+  quantidade INT NOT NULL,
+  cliente_id INT NOT NULL,
+  categoria VARCHAR(255) NOT NULL
+  total_vendas DECIMAL(10, 2) NOT NULL,
+);
